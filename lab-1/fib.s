c56bf13a23601820299c44f8d6786d93f4d465bc
Cheney <victor22771659@hotmail.com>
2014-09-29 13:08:28 +0800

    lab-1 finished

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,33 +11,38 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push { r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
+	subs r4,r0,#0
 	@ if(R0 <= 0) goto .L3 (which returns 0)
-
+	ble .L3
 	@ Compare R4 wtih 1
+	cmp r4,#1
 	@ If R4 == 1 goto .L4 (which returns 1)
-
+	beq .L4
 	@ R0 = R4 - 1
+	subs r0,r4,#1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
-
+	bl fibonacci
 	@ R5 = R0
+	mov r5,r0
 	@ R0 = R4 - 2
+	subs r0,r4,#2
 	@ Recursive call to fibonacci with R4 - 2 as parameter
-
+	bl fibonacci
 	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	adds r0,r5,r0
+	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r4, r5, pc}		@ EPILOG
 
 .L4:
 	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
